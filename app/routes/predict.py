457b3a85290acf12f1184bb8ec36d38bcbd9d7b1
Niveditha T <nivedithaindia@gmail.com>
2025-06-27 13:34:22 +0100

    Day 3: Added predict_proba, validation, and logging

--- app/routes/predict.py
+++ app/routes/predict.py
@@ -1,23 +1,37 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 import joblib
+import logging
+
+router = APIRouter()
 
 # Load the trained model and vectorizer
 model = joblib.load("model/model.pkl")
 vectorizer = joblib.load("model/vectorizer.pkl")
 
-router = APIRouter()
-
 # Input data structure
 class NewsInput(BaseModel):
     text: str
 
+logging.basicConfig(level=logging.INFO)
+
 @router.post("/predict")
 def predict(input: NewsInput):
-    if not input.text.strip():
-        return {"error": "Input text is empty"}
+    if not input.text or input.text.strip() == "":
+        return {"error": "Input text is empty or invalid."}
 
     # Vectorize the input text
     X = vectorizer.transform([input.text])
+    # prediction = model.predict(X)[0]
+    # return {"prediction": int(prediction)}
+
     prediction = model.predict(X)[0]
-    return {"prediction": int(prediction)}
+    probablity = model.predict_proba(X)[0][1]
+
+    logging.into(f"Input: {input.text}")
+    logging.info(f"Prediction: {prediction}, Probablity: {probablity}")
+
+    return {
+        "prediction": int(prediction),
+        "probablity": float(probablity)
+    }
